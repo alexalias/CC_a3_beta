39eb266a83ac530d57f29fb8f26c94d2e412c94d
Anne <aloren02@students.uni-mainz.de>
2016-06-15 13:31:32 +0200

    SettingsActivity umgebaut

--- app/src/main/java/digitale_stadt/cc_a3/SettingsActivity.java
+++ app/src/main/java/digitale_stadt/cc_a3/SettingsActivity.java
@@ -3,6 +3,8 @@ package digitale_stadt.cc_a3;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -20,51 +22,11 @@ import android.widget.Toast;
  * Diese Klasse beinhaltet die Settings (gespeichert über Shared Preferences)
  */
 
-public class SettingsActivity extends Activity{
-
-    private CheckBox checkBox_wlan;
-
-    // Shared Preferences um Einstellungen zu speichern
-    private SharedPreferences sharedPrefs;
+public class SettingsActivity extends PreferenceActivity {
 
     public void onCreate(Bundle savedInstance) {
 
         super.onCreate(savedInstance);
-        setContentView(R.layout.activity_settings);
-
-        //initialize elements
-        initializeComponents();
-
-        //attach listeners to check for changes in state
-        attachListeners();
-    }
-
-    private void initializeComponents() {
-
-        sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        checkBox_wlan = (CheckBox) findViewById(R.id.checkBox_wlan);
-        checkBox_wlan.setChecked(sharedPrefs.getBoolean("wlan_upload", false));
-    }
-
-    private void attachListeners() {
-
-        checkBox_wlan.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                if (compoundButton.isChecked()) {
-                    SharedPreferences.Editor editor = sharedPrefs.edit();
-                    editor.putBoolean("wlan_upload", true);
-                    editor.apply();
-                } else {
-                    SharedPreferences.Editor editor = sharedPrefs.edit();
-                    editor.putBoolean("wlan_upload", false);
-                    editor.apply();
-                }
-            }
-        });
-    }
-
-    public Boolean getWlanOption() {
-        return sharedPrefs.getBoolean("wlan_upload", false);
+        addPreferencesFromResource(R.layout.activity_settings);
     }
 }
